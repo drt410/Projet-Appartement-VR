1fe4b856ce59f484b055df4a4278ca4f2dd5b99d
drt410 <nicolas.tran410@gmail.com>
2019-03-18 11:09:29 +0100

    Update LevePersonneController.cs

--- Appartement Virtuel/Demo/Assets/Script/Leve personne/LevePersonneController.cs
+++ Appartement Virtuel/Demo/Assets/Script/Leve personne/LevePersonneController.cs
@@ -8,37 +8,14 @@ public class LevePersonneController : MonoBehaviour {
 	public PlayableDirector plDirector;
 	public KeyCode deplacer;
 
-
 	void OnTriggerStay(Collider other)
 	{
 		//print ("test");
 
-		if (Input.GetKeyDown (deplacer) || Input.GetKeyDown("t")) {
+		if (Input.GetKeyDown (deplacer) || Input.GetKeyDown("t")) 
+		{
 
 			plDirector.Play ();
 		}
-		}
-	/*
-    public void play()
-    {
-        foreach (PlayableDirector playableDirector in playableDirectors)
-        {
-            playableDirector.Play();
-        }
-    }
-
-    public void playAnimTimeline(int index)
-    {
-        TimelineAsset selectedAsset;
-        if (timelines.Count <= index)
-        {
-            selectedAsset = timelines[timelines.Count - 1];
-        }
-        else
-        {
-            selectedAsset = timelines[index];
-        }
-        playableDirectors[0].Play(selectedAsset);
-
-    }*/
+	}
 }
