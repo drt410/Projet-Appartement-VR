d8f439792e46d88624c76046f62d2fa8003958b7
drt410 <nicolas.tran410@gmail.com>
2019-03-18 11:08:16 +0100

    Update PersonneVue.cs

--- Appartement Virtuel/Demo/Assets/Script/Personne/PersonneVue.cs
+++ Appartement Virtuel/Demo/Assets/Script/Personne/PersonneVue.cs
@@ -10,29 +10,8 @@ public class PersonneVue : MonoBehaviour {
     public Transform body;
     public float amount = 1.0f;
 
-
-	/*public float speed = 0.001f;
-    public float distanceVue;
-
-    private Animator anim;
-
-	public KeyCode moveDown;
-	public KeyCode moveUp;
-	public KeyCode monter;
-	public KeyCode deplacer;
-	public PlayableDirector director;
-    public PlayableDirector director2;
-
-    private bool ranger = true;
-	private float countdown;
-	private float current = 0.0f;
-
-    RaycastHit hit;*/
-
     void Start()
     {
-       
-
        
     }
 
@@ -130,75 +109,4 @@ public class PersonneVue : MonoBehaviour {
         transform.rotation = Quaternion.Euler(targetRot);
         body.rotation = Quaternion.Euler(targetRotBody);
     }
-
-
-    /*
-        IEnumerator Timer(Animator animator)
-        {
-            countdown = 5.0f * Time.deltaTime;
-
-            while (countdown >= current) {
-                //countdown+= Time.deltaTime*countdown;
-                countdown--;
-            }
-                //countdown = count;
-                //Debug.Log (countdown);
-            if (countdown <= current && ranger == true) {
-                    //Debug.Log ("rangement " + hit.collider.gameObject.name);
-
-                    ranger = false;
-                    countdown = 5.0f * Time.deltaTime;;
-                    yield return new WaitForSeconds(3);
-                    animator.SetBool ("ranger", ranger);
-                    animator.Play ("deplacer");
-                    Debug.Log (ranger);
-                StopCoroutine (Timer (animator));
-
-                    //countdown = 5.0f;
-                    //animator.Play ("deplacer");
-                    //StopCoroutine (Timer (animator));
-
-                }
-            if (countdown <= current && ranger == false) {
-                    //StopCoroutine (Timer ());
-                    //yield return new WaitForSeconds (2);
-                ranger = true;
-                Debug.Log(ranger);
-                countdown = 5.0f * Time.deltaTime;;
-                yield return new WaitForSeconds (3);
-                animator.SetBool ("ranger", ranger);
-                Debug.Log (ranger);
-                StopCoroutine (Timer (animator));
-
-                }
-
-
-        }*/
-
-
 }
-
-/*Transform camera = Camera.main.transform;
-        Ray ray = new Ray(this.transform.position, this.transform.forward*distanceVue);
-        RaycastHit hit;
-        PointerEventData data = new PointerEventData(EventSystem.current);
-        GameObject obj = null;
-        if (Physics.Raycast(ray, out hit))
-        {
-            if (hit.transform.gameObject.tag == "interactible")
-            {
-                obj = hit.transform.parent.gameObject;
-            }
-        }
-        if (currentObj != obj)
-        {
-            if (currentObj != null)
-            {
-                ExecuteEvents.Execute<IPointerExitHandler>(currentObj, data, ExecuteEvents.pointerExitHandler);
-            }
-            currentObj = obj;
-            if (currentObj != null)
-            {
-                ExecuteEvents.Execute<IPointerEnterHandler>(currentObj, data, ExecuteEvents.pointerEnterHandler);
-            }
-        }*/
