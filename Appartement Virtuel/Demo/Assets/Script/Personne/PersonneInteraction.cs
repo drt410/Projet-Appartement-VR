c1613eba4312ea7da8ffe6cfee244c8908497bd4
drt410 <nicolas.tran410@gmail.com>
2019-09-09 21:29:22 +0200

    optimization

--- Appartement Virtuel/Demo/Assets/Script/Personne/PersonneInteraction.cs
+++ Appartement Virtuel/Demo/Assets/Script/Personne/PersonneInteraction.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using System;
 public class PersonneInteraction : MonoBehaviour {
 
     private GameObject[] supports;
@@ -12,6 +13,7 @@ public class PersonneInteraction : MonoBehaviour {
     public KeyCode moveDown;
     public KeyCode moveUp;
     public KeyCode deplacer;
+    public KeyCode douche;
     public PlayableDirector director;
     public PlayableDirector director2;
 
@@ -19,187 +21,216 @@ public class PersonneInteraction : MonoBehaviour {
     private float countdown;
     private float current = 0.0f;
 
+    private Animator anim;
+
     RaycastHit hit;
- 	
-	// Update is called once per frame
-	void Update () {
+
+    void Start()
+    {
+        supports = GameObject.FindGameObjectsWithTag("support_meuble");
+    }
+
+    // Update is called once per frame
+    void Update () {
+
         Debug.DrawRay(this.transform.position, this.transform.forward * distanceVue, Color.magenta);
 
         if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, distanceVue))
         {
-            Animator anim = hit.collider.gameObject.GetComponent<Animator>();
+            anim = hit.collider.gameObject.GetComponent<Animator>();
             director = hit.collider.gameObject.GetComponent<PlayableDirector>();
+            Transform corps = hit.collider.gameObject.transform;
+            GameObject meuble;
+            meuble = hit.collider.gameObject;
 
-            if (hit.collider.gameObject.tag == "leve_personne")
+            switch (meuble.tag)
             {
-                print("douche");
-
-                if (Input.GetKeyDown("l"))
-                {
-                    director.Play();
-                }
-            }
-            if (hit.collider.gameObject.tag == "interactible")
-            {
-                if (Input.GetKeyDown(deplacer) && anim.GetBool("ranger")== true)
-                {
-                    anim.Play("ouvrir");
-                    anim.SetBool("ranger", false);
-                }
-                else if (Input.GetKeyDown(deplacer) && anim.GetBool("ranger") == false)
-                {
-                    anim.SetBool("ranger", true);
-                }
-            }
-
-            if (hit.collider.gameObject.tag == "interactible_store")
-            {
-                Debug.Log("store " + hit.collider.gameObject.name);
-                if (Input.GetKeyDown(moveUp))
-                {
-                    anim.Play("ouvrir");
-                    Debug.Log("store ouvert" + hit.collider.gameObject.name);
-                    anim.SetBool("fermer", false);
-                }
-                else if (Input.GetKeyDown(moveDown))
-                {
-                    anim.SetBool("fermer", true);
-                }
+                case "interactible":
+                    AnimInteract(hit, deplacer, "ranger", "ouvrir");
+                    break;
+                case "interactible_store":
+                    AnimInteractStore(hit, moveUp, moveDown, "fermer", "ouvrir");
+                    break;
+                case "interactible_store2":
+                    AnimInteractStore(hit, moveUp, moveDown, "fermer", "ouvrir_storeC");
+                    break;
+                case "interactible_porte":
+                    AnimInteract(hit, deplacer, "ranger", "ouvrir");
+                    break;
+                case "interactible_porte2":
+                    AnimInteractPorte(hit, deplacer);
+                    break;
+                case "interactible_plt":
+                    AnimInteractPLT(hit, moveUp, moveDown, corps);
+                    break;
+                case "interactible_plt2":
+                    AnimInteractPLT(hit, moveUp, moveDown, corps);
+                    break;
+                case "meuble_haut":
+                    AnimInteractMeubleHaut(hit, moveDown, moveUp, corps);
+                    break;
+                case "leve_personne":
+                    AnimInteractLevePers(hit,KeyCode.L);
+                    break;
             }
 
-            if (hit.collider.gameObject.tag == "interactible_store2")
+            //AnimInteract(hit, deplacer, "fermer", "porte1");
+            //AnimInteractStore(hit, moveUp, moveDown, "fermer", "ouvrir");
+            //AnimInteractStore(hit, moveUp, moveDown, "fermer", "ouvrir_storeC");
+            //AnimInteractPorte(hit, deplacer);
+            //AnimInteractPLT(hit, moveUp, moveDown, corps);
+            //AnimInteractMeubleHaut(hit, moveDown, moveUp, corps);
+
+        }
+    }
+
+    void AnimInteract(RaycastHit hit,KeyCode key,string animBool, string animTrigger)
+    {
+        if(anim != null)
+        {
+            if (hit.collider.gameObject.tag != null)
             {
-                Debug.Log("store " + hit.collider.gameObject.name);
-                if (Input.GetKeyDown(moveUp))
+                if (Input.GetKeyDown(key) && anim.GetBool(animBool) == true)
                 {
-                    anim.Play("ouvrir_storeC");
-                    Debug.Log("store ouvert" + hit.collider.gameObject.name);
-                    anim.SetBool("fermer", false);
+                    anim.Play(animTrigger);
+                    anim.SetBool(animBool, false);
                 }
-                else if (Input.GetKeyDown(moveDown))
+                else if (Input.GetKeyDown(deplacer) && anim.GetBool(animBool) == false)
                 {
-                    anim.SetBool("fermer", true);
+                    anim.SetBool(animBool, true);
                 }
             }
+        }
 
-            if (hit.collider.gameObject.tag == "interactible_porte")
+    }
+
+    void AnimInteractStore(RaycastHit hit, KeyCode key1, KeyCode key2, string animBool, string animTrigger)
+    {
+        if (anim != null)
+        {
+            if (hit.collider.gameObject.tag != null)
             {
-                //  Debug.Log("porte " + hit.collider.gameObject.name);
-                if (Input.GetKeyDown(deplacer) && anim.GetBool("fermer") == true)
+                
+                if (Input.GetKeyDown(key1))
                 {
-
-                    anim.Play("porte1");
-                    anim.SetBool("fermer", false);
-                    print(anim.GetBool("fermer"));
+                    anim.Play(animTrigger);
+                    anim.SetBool(animBool, false);
                 }
-                else if (Input.GetKeyDown(deplacer) && anim.GetBool("fermer") == false)
+                else if (Input.GetKeyDown(key2))
                 {
-                    anim.SetBool("fermer", true);
+                    anim.SetBool(animBool, true);
                 }
             }
+        }
+    }
 
-            if (hit.collider.gameObject.tag == "interactible_porte2")
+    void AnimInteractPorte(RaycastHit hit, KeyCode key)
+    {
+           
+            if (hit.collider.gameObject.tag != null)
             {
-                director2 = hit.collider.gameObject.GetComponent<PlayableDirector>();
-                print("portedouche");
+            director2 = hit.collider.gameObject.GetComponent<PlayableDirector>();
 
-                if (Input.GetKeyDown(deplacer))
+            if (Input.GetKeyDown(key))
                 {
                     director2.Play();
                 }
             }
+        
+    }
 
-            if (hit.collider.gameObject.tag == "interactible_rgt")
-            {
-                //StartCoroutine (Timer (anima));
-                if (Input.GetKeyDown(deplacer))
-                {
-                    anim.Play("deplacer");
-                    anim.SetBool("ranger", false);
-                }
-                else if (Input.GetKeyDown(deplacer))
-                {
-                    anim.SetBool("ranger", true);
-                }
-            }
-            //StopCoroutine (Timer (anim));
-
+    void AnimInteractPLT(RaycastHit hit, KeyCode key1, KeyCode key2, Transform t)
+    {
+ 
             if (hit.collider.gameObject.tag == "interactible_plt")
             {
-                //Debug.Log("plan de travail "+hit.collider.gameObject.name);
-                Transform corps = hit.collider.gameObject.transform;
 
-                if (Input.GetKey(moveDown))
+                if (Input.GetKey(key2))
                 {
-                    corps.Translate(0, 0, -speed);
+                    t.Translate(0, 0, -speed);
                 }
 
-                if (Input.GetKey(moveUp))
+                if (Input.GetKey(key1))
                 {
-                    corps.Translate(0, 0, speed);
+                    t.Translate(0, 0, speed);
                 }
             }
 
-            if (hit.collider.gameObject.tag == "interactible_plt2")
+            else if (hit.collider.gameObject.tag == "interactible_plt2")
             {
-                //Debug.Log("plan de travail "+hit.collider.gameObject.name);
-                Transform corps = hit.collider.gameObject.transform;
 
                 if (Input.GetKey(moveDown))
                 {
-                    corps.Translate(0, -speed, 0);
+                    t.Translate(0, -speed, 0);
                 }
 
                 if (Input.GetKey(moveUp))
                 {
-                    corps.Translate(0, speed, 0);
+                    t.Translate(0, speed, 0);
                 }
             }
+        
+    }
 
+    void AnimInteractMeubleHaut(RaycastHit hit, KeyCode key1, KeyCode key2, Transform t)
+    {
             if (hit.collider.gameObject.tag == "meuble_haut")
-            {
-                //Debug.Log("plan de travail "+hit.collider.gameObject.name);
-                Transform corps = hit.collider.gameObject.transform;
-                supports = GameObject.FindGameObjectsWithTag("support_meuble");
-                if (Input.GetKey(moveDown))
+            {                      
+                if (Input.GetKey(key1))
                 {
                     foreach (GameObject support in supports)
                     {
                         if (support.transform.localScale.z < 60.0f)
                         {
                             support.transform.localScale += new Vector3(0, 0, speed);
-                            corps.Translate(0, speed, -speed);
+                            t.Translate(0, speed, -speed);
                         }
                     }
                 }
 
-                if (Input.GetKey(moveUp))
+                if (Input.GetKey(key2))
                 {
                     foreach (GameObject support in supports)
                     {
                         if (support.transform.localScale.z > 6.0f)
                         {
                             support.transform.localScale -= new Vector3(0, 0, speed);
-                            corps.Translate(0, -speed, speed);
+                            t.Translate(0, -speed, speed);
                         }
                     }
                 }
             }
+    }
 
-            if (hit.collider.gameObject.tag == "interactible_lit_douche")
-            {
-                Debug.Log(hit.collider.gameObject.name);
-                if (Input.GetKeyDown(moveDown))
-                {
-                    anim.Play("descendre");
-                    anim.SetBool("ranger", false);
-                }
-                else if (Input.GetKeyDown(moveUp))
-                {
-                    anim.SetBool("ranger", true);
-                }
-            }
-
+    void AnimInteractLevePers(RaycastHit hit, KeyCode key)
+    {
+        if (Input.GetKeyDown(key))
+        {
+            director.Play();
         }
     }
+
+    //if (hit.collider.gameObject.tag == "interactible_porte2")
+    //{
+    //    director2 = hit.collider.gameObject.GetComponent<PlayableDirector>();
+    //    print("portedouche");
+
+    //    if (Input.GetKeyDown(deplacer))
+    //    {
+    //        director2.Play();
+    //    }
+    //}
+
+    //if (hit.collider.gameObject.tag == "interactible_rgt")
+    //{
+    //    if (Input.GetKeyDown(deplacer))
+    //    {
+    //        anim.Play("deplacer");
+    //        anim.SetBool("ranger", false);
+    //    }
+    //    else if (Input.GetKeyDown(deplacer))
+    //    {
+    //        anim.SetBool("ranger", true);
+    //    }
+    //}
 }
